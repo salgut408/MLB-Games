e584d2512ce2f4a8d04ea7afdb8b5f35f9d316d3
salgut408 <sgutie@saic.edu>
2022-08-25 11:22:28 -0500

    Firebase crashlytics added

--- app/src/main/java/com/example/android/gamesredo/repository/GameRepository.kt
+++ app/src/main/java/com/example/android/gamesredo/repository/GameRepository.kt
@@ -235,14 +235,7 @@ class GameRepository @Inject constructor(
     }
 
 
-    // where this go
-//        val jsonFileString = Constants.getJsonDataFromAsset(this.context!!.applicationContext, "mlbcolor.json")
-//        Log.i("data", jsonFileString ?: "NOTHING")
-//        val gson = Gson()
-//        val listMlbColorType = object : TypeToken<List<MlbColors>>() {}.type
-//        var colors: List<MlbColors> = gson.fromJson(jsonFileString, listMlbColorType)
-//        colors.forEachIndexed { idx, mlbColors -> Log.i("data",">Item $idx: /n${mlbColors}") }
-//
+
 
 
 }
